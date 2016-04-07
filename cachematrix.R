5dca45f31f657de219ac1319189d297b20ea9e72
PruebasEdgar <contacto@analisisdedecisiones.com>
2016-04-07 12:30:45 -0500

    First commit: creating makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2: Lexical Scoping
+## 
+## This script contains two functions: makeCacheMatrix and cacheSolve. These function are used 
+## to create a special matrix object and calculate its inverse. The functions allow to caching ## both ## matrix and inverse in an environment that is different from the current environment.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special matrix object and associates four functions to it: set, ## ## get, setinverse and getinverse, where "inverse" is inverse of the matrix.
+## Returned objetc: LIST CLASS
 
 makeCacheMatrix <- function(x = matrix()) {
-
+			inv_mat = NULL
+			
+			set <- function(m) {
+				mat <<- m
+				inv_mat <<- NULL
+			}
+			
+			get <- function() mat
+			set_inv <- function(inv) inv_mat <<- inv
+			get_inv <- function() inv_mat
+			list(set = set, get = get, set_inv = set_inv,
+			     get_inv = get_inv) 	
+			
 }
 
 
